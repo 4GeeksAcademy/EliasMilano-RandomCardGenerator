5a26b12bb515a94ec6c69a30dff177d01f16d86b
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-09-30 03:33:35 +0000

    Added button to generate random cards

--- src/app.js
+++ src/app.js
@@ -3,46 +3,52 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  //write your code here
-  let colorList = ["black", "red"];
-  let suiteList = ["♦", "♥", "♠", "♣"];
-  let cardList = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K",
-    "A"
-  ];
+  function getRandomCard() {
+    //write your code here
+    let colorList = ["black", "red"];
+    let suiteList = ["♦", "♥", "♠", "♣"];
+    let cardList = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
 
-  function getRandom(array) {
-    let randomItem = Math.floor(Math.random() * array.length);
-    return array[randomItem];
-  }
-
-  function getRandomColor() {
-    let color = getRandom(colorList);
-    if (color == "red") {
-      return "text-danger";
-    } else {
-      return "text-black";
+    function getRandom(array) {
+      let randomItem = Math.floor(Math.random() * array.length);
+      return array[randomItem];
     }
+
+    function getRandomColor() {
+      let color = getRandom(colorList);
+      if (color == "red") {
+        return "text-danger";
+      } else {
+        return "text-black";
+      }
+    }
+
+    let randomColor = getRandomColor();
+    let randomSuite = getRandom(suiteList);
+
+    document.querySelector("#text-header").innerHTML = randomSuite;
+    document.querySelector("#text-header").classList = randomColor;
+    document.querySelector("#text-footer").innerHTML = randomSuite;
+    document.querySelector("#text-footer").classList = randomColor;
+    document.querySelector("#text-body").innerHTML = getRandom(cardList);
   }
+  getRandomCard();
 
-  let randomColor = getRandomColor();
-  let randomSuite = getRandom(suiteList);
-
-  document.querySelector("#text-header").innerHTML = randomSuite;
-  document.querySelector("#text-header").classList = randomColor;
-  document.querySelector("#text-footer").innerHTML = randomSuite;
-  document.querySelector("#text-footer").classList = randomColor;
-  document.querySelector("#text-body").innerHTML = getRandom(cardList);
+  let buttonManual = document.querySelector("button");
+  buttonManual.addEventListener("click", getRandomCard);
 };
