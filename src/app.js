2e3e2942c41d9ca49f0195793a8671f34c7575b2
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-09-30 00:53:53 +0000

    first card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(
+    "Hello this is a string extremelyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy large!!!!!!! Oh noooooooooooooooooooooooo"
+  );
 };
